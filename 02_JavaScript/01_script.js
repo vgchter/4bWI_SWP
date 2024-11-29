bc7152237a3405c4e714fcd6f1579e13fe2a2ffd
vgchter <158270323+vgchter@users.noreply.github.com>
2024-11-29 15:21:15 +0100

    Neue Ordnerstruktur

---
+++ 02_JavaScript/01_script.js
@@ -0,0 +1,53 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("A hat den Wert " + a);
+
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+//*, /, +, -
+
+c = b - a;
+console.log("b - a = " + c);
+
+let result = a + b + c;
+console.log("Das Ergebni lautet: " + result)
+
+result = (a + b) / c
+console.log("Das Ergebni lautet: " + result)
+
+result = c - a + b
+console.log("Das Ergebni lautet: " + result)
+
+result = a + b + c * 3;
+console.log("Das Ergebni lautet: " + result)
+
+result = (a + b + c) / 4;
+console.log("Das Ergebni lautet: " + result)
+
+let a1 = 3;
+let b1 = a + 3;
+let c1 = c - 3;
+
+let result1 = a + b + c;
+console.log("Das Ergebni lautet: " + result)
+
+result = (a + b) / c
+console.log("Das Ergebni lautet: " + result)
+
+result = c - a + b
+console.log("Das Ergebni lautet: " + result)
+
+result = a + b + c * 3;
+console.log("Das Ergebni lautet: " + result)
+
+result = (a + b + c) / 4;
+console.log("Das Ergebni lautet: " + result)
