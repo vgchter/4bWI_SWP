5fa1d956d55fa2355ecfa04b1688f45edcef5fbd
vgchter <158270323+vgchter@users.noreply.github.com>
2024-11-29 16:28:13 +0100

    02_Strings_&_Zahlen done

---
+++ 02_JavaScript/02_Strings_&_Zahlen/02_script.js
@@ -0,0 +1,22 @@
+let a = 7.123
+let b = 2.854
+
+let result = a + b
+console.log("a+b=" + result)
+
+let name = "Peter";
+console.log("name: " + name)
+
+console.log("Das Ergebnis von " + a + " plus " + b + " ist " + result)
+
+//Aufgabe aus dem Video:
+//Ergebnis von 17.4 / 3.9
+let c = 17.4
+let d = 3.9
+result = c / d
+console.log("The result of " + c + " divided by " + d + " is " + result)
+
+
+let vorname = "Peter"
+let nachname = "Gsteu"
+console.log("Ich heiße " + vorname + " " + nachname)
