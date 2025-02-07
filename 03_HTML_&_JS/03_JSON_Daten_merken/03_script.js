25f6afbaae06ecfe68279898fa60b991d8d5802d
vgchter <158270323+vgchter@users.noreply.github.com>
2025-02-07 15:54:03 +0100

    03_JSON_Daten_merken

---
+++ 03_HTML_&_JS/03_JSON_Daten_merken/03_script.js
@@ -0,0 +1,16 @@
+let data = [
+    { firstName: "Hans", lastName: "Huber", points: 30 },
+    { firstName: "Paul", lastName: "Müller", points: 34 },
+    { firstName: "Franz", lastName: "Maier", points: 35 }
+]
+
+
+
+function loadPeople() {
+    let html = "";
+    data.forEach(element => {
+        html += "<div>" + element.firstName + " " + element.lastName + "</div>";
+    });
+}
+
+loadPeople();
