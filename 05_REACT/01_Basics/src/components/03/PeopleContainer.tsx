e38621fcc390c6ff09f1ecff058ca6458aefa914
vgchter <158270323+vgchter@users.noreply.github.com>
2025-04-22 08:50:54 +0200

    03_Demo_Projekt_Cards_Daten done

--- 05_REACT/01_Basics/src/components/03/PeopleContainer.tsx
+++ 05_REACT/01_Basics/src/components/03/PeopleContainer.tsx
@@ -1,21 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
 type Props = {}
 
 export default function PeopleContainer({ }: Props) {
+    const [people, setPeople] = useState([]);
 
     useEffect(() => {
-        fetch("https://67b89061699a8a7baef48561.mockapi.io/person").then(res => res.json().then(data => { }))
+        fetch("https://67b89061699a8a7baef48561.mockapi.io/person").then(
+            (res) => res.json().then(data => {
+                setPeople(data);
+            })
+        )
     }, [])
 
     return (
         <div>
             <h1>People</h1>
-            <div className='w-40'>
-                <Card name="Hans" title="CEO" imageUri="https://picsum.photos/200/300" />
-                <Card name="Hans" title="CEO" imageUri="https://picsum.photos/200/300" />
-                <Card name="Hans" title="CEO" imageUri="https://picsum.photos/200/300" />
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
+                {people.map(person => {
+                    return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle}></Card>
+                })}
             </div>
         </div>
     )
