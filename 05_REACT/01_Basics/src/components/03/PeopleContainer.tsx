f49ac7f2bcbac0031f0b9b7d09b82fcd0a1eeeec
vgchter <158270323+vgchter@users.noreply.github.com>
2025-05-06 09:16:11 +0200

    05_demo_filter done

--- 05_REACT/01_Basics/src/components/03/PeopleContainer.tsx
+++ 05_REACT/01_Basics/src/components/03/PeopleContainer.tsx
@@ -1,30 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import Card from './Card'
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 type Props = {}
 
 export default function PeopleContainer({ }: Props) {
     const [people, setPeople] = useState([]);
-    const [color, setColor] = useState("green");
+    const [filteredPeople, setfilteredpeople] = useState([]);
 
     useEffect(() => {
         fetch("https://67b89061699a8a7baef48561.mockapi.io/person").then(
             (res) => res.json().then(data => {
                 setPeople(data);
+                setfilteredpeople(data);
             })
         );
     }, []);
 
-    useEffect(() => {
-        alert("change");
-    }, [color])
+    const filterPeople = (filter) => {
+        let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+        setfilteredpeople(filtered);
+    }
 
     return (
         <div>
-            <h1>People</h1>
-            <button onClick={() => { setColor("orange") }}>Change Color</button>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-                {people.map(person => {
+            <div className='fixed flex border mb-4 bg-white w-full h-25 pl-5 pr-5 items-center rounded-b-md'>
+                <input className="border p-4 rounded-lg h-fit" type='text' placeholder='Search' onChange={(el) => {
+                    console.log(el.target.value);
+                    filterPeople(el.target.value);
+                }} />
+                <h1 className='font-bold text-4xl p-2 ml-83'>People</h1>
+            </div>
+
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 pt-28 pl-5 pr-5'>
+                {filteredPeople.map(person => {
                     return <Card name={person.name} imageUri={person.avatar} title={person.jobtitle}></Card>
                 })}
             </div>
