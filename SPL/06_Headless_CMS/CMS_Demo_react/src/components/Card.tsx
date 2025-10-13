374e4568163b49215cf5d7a731995c745d52a8bc
vgchter <158270323+vgchter@users.noreply.github.com>
2025-10-13 10:39:56 +0200

    done

--- SPL/06_Headless_CMS/CMS_Demo_react/src/components/Card.tsx
+++ SPL/06_Headless_CMS/CMS_Demo_react/src/components/Card.tsx
@@ -4,13 +4,14 @@ type Props = { name: string; title: string; imageUri: string };
 
 export default function Card({ name, title, imageUri }: Props) {
   return (
-    <div className="border-1 h-40">
+    <div className="bg-slate-100 rounded-lg shadow-md overflow-hidden w-60">
+      {/* Bild oben */}
+      <img src={imageUri} alt={name} className="w-full h-60 object-cover" />
+
+      {/* Textbereich */}
       <div className="p-3">
-        <h2 className="font-bold text-xl">{name}</h2>
-        <h4>{title}</h4>
-      </div>
-      <div className="">
-        <img src={imageUri} alt="" className="w-50 h-50" />
+        <h2 className="font-bold text-sm mb-1">{name}</h2>
+        <p className="text-xs text-gray-700 leading-tight">{title}</p>
       </div>
     </div>
   );
--- SPL/06_Headless_CMS/CMS_Demo_react/src/components/PeopleContainer.tsx
+++ SPL/06_Headless_CMS/CMS_Demo_react/src/components/PeopleContainer.tsx
@@ -1,54 +1,36 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
-type Props = {};
-type Person = { name: string; avatar: string; jobtitle: string };
+type Person = { id: string; name: string; avatar: string; jobtitle: string };
 
-export default function PeopleContainer({}: Props) {
-  const [people, setPeople] = useState([]);
-  const [filteredPeople, setfilteredpeople] = useState<Person[]>([]);
+export default function PeopleContainer() {
+  const [people, setPeople] = useState<Person[]>([]);
 
   useEffect(() => {
-    fetch("https://67b89061699a8a7baef48561.mockapi.io/person").then((res) =>
-      res.json().then((data) => {
-        setPeople(data);
-        setfilteredpeople(data);
-      })
-    );
+    fetch("https://67b89061699a8a7baef48561.mockapi.io/person")
+      .then((res) => res.json())
+      .then((data) => setPeople(data));
   }, []);
 
-  const filterPeople = (filter: string) => {
-    let filtered = people.filter((person: Person) =>
-      person.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    setfilteredpeople(filtered);
-  };
-
   return (
-    <div>
-      <div className="fixed flex border mb-4 bg-white w-full h-25 pl-5 pr-5 items-center rounded-b-md">
-        <input
-          className="border p-4 rounded-lg h-fit"
-          type="text"
-          placeholder="Search"
-          onChange={(el) => {
-            console.log(el.target.value);
-            filterPeople(el.target.value);
-          }}
-        />
-        <h1 className="font-bold text-4xl p-2 ml-83">People</h1>
+    <div className="min-h-screen bg-[#2f3308]">
+      {/* Header */}
+      <div className="flex justify-center items-center py-6 bg-[#2f3308]">
+        <h1 className="font-extrabold text-4xl md:text-5xl text-white">
+          HTL Dornbirn 5bWI
+        </h1>
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 pt-28 pl-5 pr-5">
-        {filteredPeople.map((person: Person) => {
-          return (
-            <Card
-              name={person.name}
-              imageUri={person.avatar}
-              title={person.jobtitle}
-            />
-          );
-        })}
+      {/* Grid mit Cards */}
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 p-8 justify-items-center">
+        {people.map((person) => (
+          <Card
+            key={person.id}
+            name={person.name}
+            title={person.jobtitle}
+            imageUri={person.avatar}
+          />
+        ))}
       </div>
     </div>
   );
