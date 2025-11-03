6ec6d52ec0507c5a67a36cebeedccbeb67d37b62
vgchter <158270323+vgchter@users.noreply.github.com>
2025-11-03 10:01:44 +0100

    update

--- SPL/06_Headless_CMS/CMS_Demo_react/src/components/Card-Website/Card.tsx
+++ SPL/06_Headless_CMS/CMS_Demo_react/src/components/Card-Website/Card.tsx
@@ -2,7 +2,10 @@ type Props = { name: string; title: string; imageUri: string };
 
 export default function Card({ name, title, imageUri }: Props) {
   return (
-    <div className="bg-slate-100 rounded-lg shadow-md overflow-hidden h-full">
+    <div
+      className="bg-slate-100 rounded-lg shadow-md overflow-hidden h-full"
+      style={{ fontFamily: "JetBrain Mono, monospace" }}
+    >
       <img src={imageUri} alt={name} className="w-full object-cover" />
       <div className="p-3">
         <h2 className="font-bold text-sm mb-1">{name}</h2>
